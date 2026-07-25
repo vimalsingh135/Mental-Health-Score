cc002e1c9223592d968de782b97c41a4db3f7aca
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-07-25 20:27:26 +0530

    changing URL in script file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "http://127.0.0.1:8000";
+  const API_BASE = "https://mental-health-score-4l58.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
